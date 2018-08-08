d38f01701d7d5e9f7f9aeeb7852cfc5f237a00e0
Joel <joelalexanderramos@gmail.com>
2018-08-07 23:05:55 -0400

    Productos 2

--- server/routes/producto.js
+++ server/routes/producto.js
@@ -13,9 +13,14 @@ const _ = require('underscore');
 // Obtener todas los productos
 // ==================================
 app.get('/producto', verificaToken, (req, res) => {
-    // paginado    
+    // paginado  
 
-    Producto.find({}, "descripcion categoria usuario")
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    Producto.find({ disponible: true }, "descripcion categoria usuario")
+        .skip(desde)
+        .limit(5)
         .populate('usuario', 'nombre email')
         .populate('categoria', 'descripcion')
         .exec((err, productos) => {
@@ -44,31 +49,64 @@ app.get('/producto/:id', verificaToken, (req, res) => {
     // populate
     let id = req.params.id;
 
-    Producto.findById(id, (err, productoDB) => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err
+    Producto.findById(id)
+        .populate('usuario', 'nombre email')
+        .populate('categoria', 'descripcion')
+        .exec((err, productoDB) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            };
+
+            if (!productoDB) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'El ID no es correcto'
+                    }
+                })
+            };
+
+            res.json({
+                ok: true,
+                producto: productoDB
             });
-        };
 
-        if (!productoDB) {
-            return res.status(400).json({
-                ok: false,
-                err: {
-                    message: 'El ID no es correcto'
-                }
-            })
-        };
-
-        res.json({
-            ok: true,
-            producto: productoDB
         });
 
-    });
+});
+
+// ==================================
+// Buscar producto
+// ==================================
+app.get('/producto/buscar/:termino', verificaToken, (req, res) => {
+
+    let termino = req.params.termino;
+
+    let regex = new RegExp(termino, 'i');
+
+    Producto.find({ descripcion: regex })
+        .populate('categoria', 'nombre')
+        .exec((err, productos) => {
+
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            };
+
+            return res.json({
+                ok: true,
+                productos
+            });
+
+        });
 
 });
+
 
 
 // ==================================
@@ -173,7 +211,8 @@ app.delete('/producto/:id', [verificaToken], (req, res) => {
 
         res.json({
             ok: true,
-            producto: productoDB
+            producto: productoDB,
+            message: 'Producto borrado'
         });
 
     });
